7d175fdfff76b495d426f869c5a1dfabb58e19a1
kriyandi <kriyandimitrov@gmail.com>
2021-07-12 14:48:52 +0300

    Change TodoApp component return

--- projects/basic-todo-app/basic-todo-app/src/TodoApp.js
+++ projects/basic-todo-app/basic-todo-app/src/TodoApp.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 import AddTodo from './components/AddTodo';
+import Filters from './components/Filters';
+import TodoList from './components/TodoList';
 
 const TodoApp = () => {
     return (
         <div>
             <Header />
             <AddTodo />
+            <Filters />
+            <TodoList />
      </div>
     );
 }
