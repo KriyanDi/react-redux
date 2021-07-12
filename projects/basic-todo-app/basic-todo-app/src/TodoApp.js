a8bcb797b793d23b3705b98f155995fc414bd796
kriyandi <kriyandimitrov@gmail.com>
2021-07-12 15:27:13 +0300

    Provide store to the entry point of the app

--- projects/basic-todo-app/basic-todo-app/src/TodoApp.js
+++ projects/basic-todo-app/basic-todo-app/src/TodoApp.js
@@ -3,15 +3,19 @@ import Header from './components/Header';
 import AddTodo from './components/AddTodo';
 import Filters from './components/Filters';
 import TodoList from './components/TodoList';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const TodoApp = () => {
     return (
-        <div>
-            <Header />
-            <AddTodo />
-            <Filters />
-            <TodoList />
-     </div>
+        <Provider store={store}>
+            <div>
+                <Header />
+                <AddTodo />
+                <Filters />
+                <TodoList />
+            </div>
+        </Provider>
     );
 }
 
