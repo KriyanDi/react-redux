7380feb7cefbe8cb2dfc00459dbdb31cc494d286
kriyandi <kriyandimitrov@gmail.com>
2021-07-12 15:54:47 +0300

    Connect AddTodo component

--- projects/basic-todo-app/basic-todo-app/src/components/AddTodo.js
+++ projects/basic-todo-app/basic-todo-app/src/components/AddTodo.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from "react-redux";
+import { addTodo } from "../redux/actions";
 
-const AddTodo = () => {
+const AddTodo = (props) => {
+    const [content, setContent] = useState("");
+
     return (
         <div>
             <div className="ui input">
-                <input type="text" placeholder="Add Todo..."/>
+                <input
+                    type="text"
+                    placeholder="Add Todo..."
+                    onChange={event => setContent(event.target.value)}
+                    value={ content}/>
             </div>
             <span>
-                <button className="ui primary button">Save</button>
+                <button
+                    className="ui primary button"
+                    onClick={() => {
+                        props.addTodo(content);
+                        setContent("");
+                    } }
+                >
+                    Save
+                </button>
             </span>
         </div>
     );
 }
 
-export default AddTodo;
+export default connect(null, { addTodo })(AddTodo);
