85e5e167db058634d9c28fbcb77316dba44463e7
kriyandi <kriyandimitrov@gmail.com>
2021-07-12 14:09:46 +0300

    Change TodoApp component

--- projects/basic-todo-app/basic-todo-app/src/TodoApp.js
+++ projects/basic-todo-app/basic-todo-app/src/TodoApp.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import AddTodo from './components/AddTodo';
 
 const TodoApp = () => {
-    return <div>TodoApp</div>;
+    return (
+        <div>
+            <Header />
+            <AddTodo />
+     </div>
+    );
 }
 
 export default TodoApp; 
