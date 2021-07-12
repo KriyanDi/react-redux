4fa4661b7f8dc60117f6ec9b431a1e27dc271994
kriyandi <kriyandimitrov@gmail.com>
2021-07-12 16:24:14 +0300

    Add onClick event handler for Filters component

--- projects/basic-todo-app/basic-todo-app/src/components/Filters.js
+++ projects/basic-todo-app/basic-todo-app/src/components/Filters.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {FILTERS} from '../constants'
+import { setFilter } from '../redux/actions';
 
 const Filters = () => {
     return (
@@ -7,7 +9,10 @@ const Filters = () => {
             {Object.keys(FILTERS).map(filterKey => {
                 const currFilter = FILTERS[filterKey];
                 return (
-                    <span key={currFilter}>
+                    <span
+                        key={currFilter}
+                        onClick={setFilter}
+                    >
                         {currFilter}{" "}
                     </span>
                 );
@@ -16,4 +21,8 @@ const Filters = () => {
     );
 }
 
-export default Filters;
+const mapStateToProps = state => {
+    return { activeFilter: state.filter };
+}
+
+export default connect(mapStateToProps, { setFilter })(Filters);
