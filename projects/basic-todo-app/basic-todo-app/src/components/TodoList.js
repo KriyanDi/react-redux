beff20259b071e462ab7cf6ce87320caacebb963
kriyandi <kriyandimitrov@gmail.com>
2021-07-13 11:27:31 +0300

    Format text in TodoList file

--- projects/basic-todo-app/basic-todo-app/src/components/TodoList.js
+++ projects/basic-todo-app/basic-todo-app/src/components/TodoList.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import TodoItem from './TodoItem'
-import { connect } from 'react-redux';
+import React from "react";
+import TodoItem from "./TodoItem";
+import { connect } from "react-redux";
 import { selectTodoByFilter, selectTodos } from "../redux/selectors";
 
 const TodoList = ({ todos }) => {
-    return (
-        <ul>
-            {todos && todos.length
-                ? todos.map(todo => {
-                    return <TodoItem key={`todo-${todo.id}`} todo={todo} />;
-                })
-                : "All done!"}
-        </ul>
-    );
-}
+  return (
+    <ul>
+      {todos && todos.length
+        ? todos.map((todo) => {
+            return <TodoItem key={`todo-${todo.id}`} todo={todo} />;
+          })
+        : "All done!"}
+    </ul>
+  );
+};
 
-const mapStateToProps = state => {
-    const { filter } = state;
-    const todos = selectTodoByFilter(state, filter);
-    return { todos };
-}
+const mapStateToProps = (state) => {
+  const { filter } = state;
+  const todos = selectTodoByFilter(state, filter);
+  return { todos };
+};
 
 export default connect(mapStateToProps)(TodoList);
