43cc8b7a47341158887cd8b2ede22c41c0b08cee
kriyandi <kriyandimitrov@gmail.com>
2021-07-13 10:13:32 +0300

    Fix AddTodo component
    Now in the input field you no longer can save an empty todo

--- projects/basic-todo-app/basic-todo-app/src/components/AddTodo.js
+++ projects/basic-todo-app/basic-todo-app/src/components/AddTodo.js
@@ -1,30 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { addTodo } from "../redux/actions";
 
 const AddTodo = (props) => {
-    const [content, setContent] = useState("");
+  const [content, setContent] = useState("");
 
-    return (
-        <div>
-            <div className="ui input">
-                <input
-                    type="text"
-                    placeholder="Add Todo..."
-                    onChange={event => setContent(event.target.value)}
-                    value={content}
-                />
-            </div>
-            <span>
-                <button
-                    className="ui primary button"
-                    onClick={content ? { addTodo(content) setContent("")}: }
-                >
-                    Save
-                </button>
-            </span>
-        </div>
-    );
-}
+  const saveHandlerOnClick = (content) => {
+    props.addTodo(content);
+    setContent("");
+  };
+
+  return (
+    <div>
+      <div className="ui input">
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          onChange={(event) => setContent(event.target.value)}
+          value={content}
+        />
+      </div>
+      <span>
+        <button
+          className="ui primary button"
+          onClick={content ? () => saveHandlerOnClick(content) : () => {}}
+        >
+          Save
+        </button>
+      </span>
+    </div>
+  );
+};
 
 export default connect(null, { addTodo })(AddTodo);
