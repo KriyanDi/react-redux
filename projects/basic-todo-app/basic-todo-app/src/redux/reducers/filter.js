5abe5c0cdc2db07a132d4c1ee0ed92377f78cba9
kriyandi <kriyandimitrov@gmail.com>
2021-07-13 11:26:08 +0300

    Format text in filter file

--- projects/basic-todo-app/basic-todo-app/src/redux/reducers/filter.js
+++ projects/basic-todo-app/basic-todo-app/src/redux/reducers/filter.js
@@ -4,14 +4,14 @@ import { FILTERS } from "../../constants";
 const initialState = FILTERS.ALL;
 
 const filter = (state = initialState, action) => {
-    switch (action.type) {
-        case SET_FILTER: {
-            return action.payload.filter;
-        }
-        default: {
-            return state;
-        }
+  switch (action.type) {
+    case SET_FILTER: {
+      return action.payload.filter;
     }
-}
+    default: {
+      return state;
+    }
+  }
+};
 
 export default filter;
