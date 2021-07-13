5e3b886c864d356630c72cd026ad4273a0bde4bc
kriyandi <kriyandimitrov@gmail.com>
2021-07-13 10:18:25 +0300

    Format text in selectors file

--- projects/basic-todo-app/basic-todo-app/src/redux/selectors.js
+++ projects/basic-todo-app/basic-todo-app/src/redux/selectors.js
@@ -1,28 +1,26 @@
 import { FILTERS } from "../constants";
 
-export const selectTodoList = store =>
-    store && store.todos
-        ? store.todos.allIds
-        : [];
+export const selectTodoList = (store) =>
+  store && store.todos ? store.todos.allIds : [];
 
 export const selectTodoById = (store, id) =>
-    store && store.todos && store.todos.byIds
-        ? { ...store.todos.byIds[id], id }
-        : {};
+  store && store.todos && store.todos.byIds
+    ? { ...store.todos.byIds[id], id }
+    : {};
 
-export const selectTodos = store =>
-    selectTodoList(store).map(id => selectTodoById(store, id));
+export const selectTodos = (store) =>
+  selectTodoList(store).map((id) => selectTodoById(store, id));
 
 export const selectTodoByFilter = (store, filter) => {
-    const allTodos = selectTodos(store);
+  const allTodos = selectTodos(store);
 
-    switch (filter) {
-        case FILTERS.COMPLETED:
-            return allTodos.filter( todo => todo.completed)
-        case FILTERS.INCOMPLETE:
-            return allTodos.filter(todo => !todo.completed)
-        case FILTERS.ALL:
-        default:
-            return allTodos;
-    }
-}
+  switch (filter) {
+    case FILTERS.COMPLETED:
+      return allTodos.filter((todo) => todo.completed);
+    case FILTERS.INCOMPLETE:
+      return allTodos.filter((todo) => !todo.completed);
+    case FILTERS.ALL:
+    default:
+      return allTodos;
+  }
+};
