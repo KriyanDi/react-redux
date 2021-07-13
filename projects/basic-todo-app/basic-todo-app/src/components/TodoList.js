c5aa2668fef7c96dae00c7b93fc20eb8a1ce0cde
kriyandi <kriyandimitrov@gmail.com>
2021-07-13 12:41:38 +0300

    Change TodoList component UI

--- projects/basic-todo-app/basic-todo-app/src/components/TodoList.js
+++ projects/basic-todo-app/basic-todo-app/src/components/TodoList.js
@@ -2,23 +2,42 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import { connect } from "react-redux";
 import { selectTodoByFilter, selectTodos } from "../redux/selectors";
+import { FILTERS } from "../constants";
 
-const TodoList = ({ todos }) => {
+const manageSelectedFilter = (filter) => {
+  console.log(filter);
+  switch (filter) {
+    case FILTERS.ALL:
+      return "All done!";
+    case FILTERS.INCOMPLETE:
+      return "There is no incomplete to-dos here!";
+    case FILTERS.COMPLETED:
+      return "There is no complete to-dos here!";
+    default:
+      return "";
+  }
+};
+
+const TodoList = ({ todos, filter }) => {
   return (
-    <ul>
-      {todos && todos.length
-        ? todos.map((todo) => {
+    <div className="ui segment">
+      <ul>
+        {todos && todos.length ? (
+          todos.map((todo) => {
             return <TodoItem key={`todo-${todo.id}`} todo={todo} />;
           })
-        : "All done!"}
-    </ul>
+        ) : (
+          <h5 className="ui header">{manageSelectedFilter(filter)}</h5>
+        )}
+      </ul>
+    </div>
   );
 };
 
 const mapStateToProps = (state) => {
   const { filter } = state;
   const todos = selectTodoByFilter(state, filter);
-  return { todos };
+  return { todos, filter };
 };
 
 export default connect(mapStateToProps)(TodoList);
