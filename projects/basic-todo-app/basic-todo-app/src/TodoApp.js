69d0a455c01ba3050925da577b3c9e65fbb376a1
kriyandi <kriyandimitrov@gmail.com>
2021-07-13 12:41:15 +0300

    Change TodoApp component

--- projects/basic-todo-app/basic-todo-app/src/TodoApp.js
+++ projects/basic-todo-app/basic-todo-app/src/TodoApp.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Header from './components/Header';
-import AddTodo from './components/AddTodo';
-import Filters from './components/Filters';
-import TodoList from './components/TodoList';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import React from "react";
+import Header from "./components/Header";
+import AddTodo from "./components/AddTodo";
+import Filters from "./components/Filters";
+import TodoList from "./components/TodoList";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const TodoApp = () => {
-    return (
-        <Provider store={store}>
-            <div>
-                <Header />
-                <AddTodo />
-                <Filters />
-                <TodoList />
-            </div>
-        </Provider>
-    );
-}
+  return (
+    <Provider store={store}>
+      <div className="ui form attached segment">
+        <Header />
+        <AddTodo />
+        <Filters />
+        <TodoList />
+      </div>
+    </Provider>
+  );
+};
 
-export default TodoApp; 
+export default TodoApp;
