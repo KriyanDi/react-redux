cc7c23b6da8a2bc46645dda86f075fdf4c54b53b
kriyandi <kriyandimitrov@gmail.com>
2021-07-13 11:24:52 +0300

    Fix logic in Filters  component

--- projects/basic-todo-app/basic-todo-app/src/components/Filters.js
+++ projects/basic-todo-app/basic-todo-app/src/components/Filters.js
@@ -1,28 +1,58 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import {FILTERS} from '../constants'
-import { setFilter } from '../redux/actions';
+import React from "react";
+import { connect } from "react-redux";
+import { FILTERS } from "../constants";
+import { setFilter } from "../redux/actions";
 
-const Filters = () => {
-    return (
-        <div>
-            {Object.keys(FILTERS).map(filterKey => {
-                const currFilter = FILTERS[filterKey];
-                return (
-                    <span
-                        key={currFilter}
-                        onClick={setFilter}
-                    >
-                        {currFilter}{" "}
-                    </span>
-                );
-            })}
-        </div>
-    );
-}
+const getStringNameOfTheNumber = (number) => {
+  switch (number) {
+    case 1:
+      return "one";
+    case 2:
+      return "two";
+    case 3:
+      return "three";
+    case 4:
+      return "four";
+    case 5:
+      return "five";
+    case 6:
+      return "six";
+    case 7:
+      return "seven";
+    case 8:
+      return "eight";
+    case 9:
+      return "nine";
+    case 10:
+      return "ten";
+    default:
+      return "";
+  }
+};
 
-const mapStateToProps = state => {
-    return { activeFilter: state.filter };
-}
+const Filters = (props) => {
+  let stringNameOfFilters =
+    getStringNameOfTheNumber(Object.keys(FILTERS).length) + " ui buttons";
+  return (
+    <div className={stringNameOfFilters}>
+      {Object.keys(FILTERS).map((filterKey) => {
+        const currFilter = FILTERS[filterKey];
+        return (
+          <button
+            key={`filter-${currFilter}`}
+            className="ui button"
+            onClick={() => props.setFilter(currFilter)}
+          >
+            {currFilter}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { activeFilter: state.filter };
+};
 
 export default connect(mapStateToProps, { setFilter })(Filters);
